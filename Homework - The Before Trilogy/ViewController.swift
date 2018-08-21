11791645e5c22f08203ce7c3df20ceec3fba7b94
Pei Wu <peiwu@peideair.home>
2018-08-21 17:05:51 +0800

    Homework#10 Before Trilogy

--- Homework - The Before Trilogy/ViewController.swift
+++ Homework - The Before Trilogy/ViewController.swift
@@ -15,6 +15,28 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var image01: UIImageView!
+    @IBOutlet weak var movieQuoteLabel: UILabel!
+    
+    @IBAction func SegControl01(_ sender: UISegmentedControl) {
+        
+        if ( sender.selectedSegmentIndex == 0){
+            image01.image = UIImage(named: "Before01")
+            movieQuoteLabel.text = "I want to keep talking to you, y'know. I have no idea what your situation is, but, uh, but I feel like we have some kind of, uh, connection."
+        }
+        
+        else if ( sender.selectedSegmentIndex == 1){
+            image01.image = UIImage(named: "Before02")
+            movieQuoteLabel.text = "I guess when you’re young, you just believe there’ll be many people with whom you’ll connect with. Later in life, you realize it only happens a few times."
+        }
+        
+        else{
+            image01.image = UIImage(named: "Before03")
+            movieQuoteLabel.text = "If you want true love, then this is it. This is real life. It’s not perfect, but it’s real."
+        }
+        
+    }
+    
 
 }
 
